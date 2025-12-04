759051c8ff6fa52d551a40290e8f79d5298a339d
Lord Drago <clearrio1465@gmail.com>
2025-12-04 20:39:05 +0530

    Refactor bot to use commands extension and add slash command

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,21 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 
-class client(discord.Client):
+class client(commands.Bot):
     async def on_ready(self):
         print(f'Logged in as {self.user}!')
-    # This function is called when the bot is ready and connected to Discord.
+
     async def on_message(self, message):
         if message.author == self.user:
             return
-        # If the message starts with 'hello', respond with a greeting.
+        
         if message.content.startswith('hello'):
             await message.channel.send(f'Hi there {message.author}')
-        # If the message starts with 'how are you', respond accordingly.
+        
         if message.content.startswith('how are you'):
             await message.channel.send(f'I\'m fine how are you, {message.author}')
-        # If a new me member joins, welcome them to the server.
+        
     async def on_member_join(self, member):
         chanel = discord.utils.get(member.guild.text_channels, name='general')
         if chanel:
@@ -22,7 +24,13 @@ class client(discord.Client):
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = client(command_prefix='!', intents=intents)
 
 
-client = client(intents=intents)
+GUILD_ID = discord.Object(id=YOUR_SERVER_ID)
+
+@client.tree.command(name='hello', description='Says hello to the user', guild=GUILD_ID)
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Hello {interaction.user.mention}!')
+
 client.run('YOUR_BOT_TOKEN')
